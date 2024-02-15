dd6964f60c911f9d92ca88e9e6de35cc1213d9e7
raistmere <raistmere@gmail.com>
2024-02-15 17:36:47 -0600

    Added base html and css styling for ProductPage
    * Added all of the html for the ProductPage
    * Added partial css styling for ProductPage
    * Added font, colors and variables
    * Added productPage icons

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
-import Product from '../Product/Product.tsx';
+import styles from "./ProductPage.module.css";
+import productImage1 from "../../assets/images/image-product-1.jpg";
+import leftArrow from "../../assets/images/icon-previous.svg";
+import rightArrow from "../../assets/images/icon-next.svg";
+import minusIcon from "../../assets/images/icon-minus.svg";
+import plusIcon from "../../assets/images/icon-plus.svg";
 
 type Props = {
   productName: string,
@@ -18,9 +23,43 @@ const ProductPage = (props: Props) => {
   }
 
   return (
-    <div className="productPage">
-      <Product productName={props.productName}/>
-      <button onClick={addToCart}>Add to Cart</button>
+    <div id={styles.productPage}>
+      <div className={styles.productImageBox}>
+        <img src={productImage1} alt="A picture of the product" className='productImage'/>
+        <div className={styles.arrowButtonBox}>
+          <button className={styles.prevButton}>
+            <img src={leftArrow} alt="A previous arrow icon" />
+          </button>
+          <button className={styles.nextButton}>
+            <img src={rightArrow} alt="A next arrow icon" />
+          </button>
+        </div>
+      </div>
+      <div className={styles.productContentBox}>
+        <div className={styles.productInfoBox}>
+          <button>SNEAKER COMPANY</button>
+          <h2>Fall Limited Edition Sneakers</h2>
+          <p>
+            These low-profile sneakers are your perfect casual wear companion. Featuring a durable
+            rubber outer sole,they'll withstand everything the weather can offer.
+          </p>
+          <div className={styles.productPriceBox}>
+            <h2>$125.00</h2>
+            <p>50%</p>
+            <p>$250.00</p>
+          </div>
+        </div>
+        <div className={styles.productQtyBox}>
+          <button>
+            <img src={minusIcon} alt="A minus icon" />
+          </button>
+          <p>0</p>
+          <button>
+            <img src={plusIcon} alt="A plus icon" />
+          </button>
+        </div>
+        <button onClick={addToCart} className={styles.addCartButton}>Add to Cart</button>
+      </div>
     </div>
   )
 }
